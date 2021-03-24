3bbe8c62bd3a6e6ff1b0c86a5c84b23b60e0db1d
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-24 17:16:51 -0300

    feito tela sign in

---
+++ android/app/src/main/java/dev/joaogabriel/bytebank/view/SignIn.kt
@@ -0,0 +1,13 @@
+package dev.joaogabriel.bytebank.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import dev.joaogabriel.bytebank.R
+
+class SignIn : Fragment(R.layout.fragment_sign_in) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
