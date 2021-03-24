9a4a7ffa050e38c90fb090f3b97114a51ed0fead
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-24 16:35:41 -0300

    feito layout da tela sign up e mudanca no background da tela splash

---
+++ android/app/src/main/java/dev/joaogabriel/bytebank/view/SignUp.kt
@@ -0,0 +1,13 @@
+package dev.joaogabriel.bytebank.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import dev.joaogabriel.bytebank.R
+
+class SignUp : Fragment(R.layout.fragment_sign_up) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
