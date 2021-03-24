a3b61b0d70b9188769cbeec3e8f2bf2b7885e670
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-23 21:24:56 -0300

    :lipstick: tela splash

---
+++ android/app/src/main/java/dev/joaogabriel/bytebank/view/Splash.kt
@@ -0,0 +1,13 @@
+package dev.joaogabriel.bytebank.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import dev.joaogabriel.bytebank.R
+
+class Splash : Fragment(R.layout.fragment_splash) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
