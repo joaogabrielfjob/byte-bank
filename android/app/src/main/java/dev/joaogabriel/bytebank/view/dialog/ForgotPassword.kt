ee59a6ea72c899ecb9ed0d9f974abeaceff92c39
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-24 21:05:26 -0300

    feito dialog forgot password

---
+++ android/app/src/main/java/dev/joaogabriel/bytebank/view/dialog/ForgotPassword.kt
@@ -0,0 +1,19 @@
+package dev.joaogabriel.bytebank.view.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import dev.joaogabriel.bytebank.R
+
+class ForgotPassword: DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+}
