058ccff6eadd68185aa9a5543f6d1c39403acaf7
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-24 21:23:23 -0300

    feito dialog add transaction

--- android/app/src/main/java/dev/joaogabriel/bytebank/view/dialog/AddTransaction.kt
+++ android/app/src/main/java/dev/joaogabriel/bytebank/view/dialog/AddTransaction.kt
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import dev.joaogabriel.bytebank.R
 
-class ForgotPassword : DialogFragment() {
+class AddTransaction : DialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+        return inflater.inflate(R.layout.fragment_add_transaction, container, false)
     }
 }
