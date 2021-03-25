0eaf5a8b4cc30ddf376a0d3dc765467bb21b8d76
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-25 18:37:33 -0300

    adicionando algumas navegacoes

--- android/app/src/main/java/dev/joaogabriel/bytebank/view/SignIn.kt
+++ android/app/src/main/java/dev/joaogabriel/bytebank/view/SignIn.kt
@@ -3,11 +3,22 @@ package dev.joaogabriel.bytebank.view
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.Navigation
 import dev.joaogabriel.bytebank.R
+import dev.joaogabriel.bytebank.databinding.FragmentSignInBinding
 
 class SignIn : Fragment(R.layout.fragment_sign_in) {
+    private lateinit var binding: FragmentSignInBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSignInBinding.bind(view)
+
+        binding.signInBtnSignUp.setOnClickListener { openSignUp() }
+    }
+
+    private fun openSignUp() {
+        Navigation.findNavController(requireView()).navigate(R.id.signInToSignUp)
     }
 }
--- android/app/src/main/java/dev/joaogabriel/bytebank/view/Splash.kt
+++ android/app/src/main/java/dev/joaogabriel/bytebank/view/Splash.kt
@@ -1,13 +1,24 @@
 package dev.joaogabriel.bytebank.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.Navigation
 import dev.joaogabriel.bytebank.R
 
 class Splash : Fragment(R.layout.fragment_splash) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            openSignIn()
+        }, 2000)
+    }
+
+    private fun openSignIn() {
+        Navigation.findNavController(requireView()).navigate(R.id.splashToSignIn)
     }
 }
