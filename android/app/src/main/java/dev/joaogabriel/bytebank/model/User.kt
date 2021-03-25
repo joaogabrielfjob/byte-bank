bfce5f055f975fe6cd2251c780885d37b4765243
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-25 20:49:10 -0300

    melhorias no objeto do usuario

--- android/app/src/main/java/dev/joaogabriel/bytebank/model/User.kt
+++ android/app/src/main/java/dev/joaogabriel/bytebank/model/User.kt
@@ -1,10 +1,10 @@
 package dev.joaogabriel.bytebank.model
 
 data class User(
-    var accountNumber: Int,
-    var email: String,
-    var password: String,
-    var name: String,
-    var balance: Float,
+    var accountNumber: Int = 0,
+    var email: String = "",
+    var password: String = "",
+    var name: String = "",
+    var balance: Float = 0f,
     var transactions: List<Transaction>? = null
 )
