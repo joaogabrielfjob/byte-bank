e165174dd6a1d44dbb9937b324be9f95a23cd6a8
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-26 12:51:46 -0300

    melhorias na splash para autenticar usuario

--- android/app/src/main/java/dev/joaogabriel/bytebank/view/Splash.kt
+++ android/app/src/main/java/dev/joaogabriel/bytebank/view/Splash.kt
@@ -5,17 +5,36 @@ import android.os.Handler
 import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import dev.joaogabriel.bytebank.R
+import dev.joaogabriel.bytebank.utils.Resource
+import dev.joaogabriel.bytebank.viewmodel.UserViewModel
 
 class Splash : Fragment(R.layout.fragment_splash) {
+    private val userViewModel: UserViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            openSignIn()
-        }, 2000)
+        userViewModel.getUserData()
+        userResponse()
+    }
+
+    private fun userResponse() {
+        userViewModel.userResponse.observe(viewLifecycleOwner, { response ->
+            when(response) {
+                is Resource.Success -> openHome()
+
+                is Resource.Error -> openSignIn()
+
+                is Resource.Loading -> println("loading splash")
+            }
+        })
+    }
+
+    private fun openHome() {
+        Navigation.findNavController(requireView()).navigate(R.id.splashToHome)
     }
 
     private fun openSignIn() {
