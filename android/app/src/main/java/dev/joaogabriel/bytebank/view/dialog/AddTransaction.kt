9123ee2566d1289f4894f6efb9523678faec39d5
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-26 10:59:47 -0300

    logica para adicionar transaction

--- android/app/src/main/java/dev/joaogabriel/bytebank/view/dialog/AddTransaction.kt
+++ android/app/src/main/java/dev/joaogabriel/bytebank/view/dialog/AddTransaction.kt
@@ -5,9 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
 import dev.joaogabriel.bytebank.R
+import dev.joaogabriel.bytebank.databinding.FragmentAddTransactionBinding
+import dev.joaogabriel.bytebank.model.Contact
+import dev.joaogabriel.bytebank.model.Transaction
+import dev.joaogabriel.bytebank.model.User
+import dev.joaogabriel.bytebank.utils.Resource
+import dev.joaogabriel.bytebank.viewmodel.UserViewModel
+import java.util.Date
 
 class AddTransaction : DialogFragment() {
+    private lateinit var binding: FragmentAddTransactionBinding
+    private lateinit var currentUser: User
+    private val userViewModel: UserViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +26,39 @@ class AddTransaction : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_add_transaction, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentAddTransactionBinding.bind(view)
+        binding.transactionBtnConfirm.setOnClickListener { addTransaction() }
+        binding.transactionBtnGoBack.setOnClickListener { dismiss() }
+
+        userResponse()
+    }
+
+    private fun userResponse() {
+        userViewModel.userResponse.observe(viewLifecycleOwner, { response ->
+            when(response) {
+                is Resource.Success -> if (response.data != null) currentUser = response.data
+
+                is Resource.Error -> println(response.message)
+
+                is Resource.Loading -> println("loading add transaction")
+            }
+        })
+    }
+
+    private fun addTransaction() {
+        val value = binding.transactionTxtValue.text.toString().toFloat()
+        val transaction = Transaction(
+            value,
+            Contact(currentUser.name, currentUser.accountNumber),
+            Date().toString()
+        )
+
+        userViewModel.addTransaction(currentUser, transaction)
+        dismiss()
     }
 }
