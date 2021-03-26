2a95d55d3d7021547c7c1e6ecfe4cb0af4709fc4
João Gabriel <joao.gabrielfjob@gmail.com>
2021-03-25 21:41:27 -0300

    populando dados do usuario na tela home

--- android/app/src/main/java/dev/joaogabriel/bytebank/view/Home.kt
+++ android/app/src/main/java/dev/joaogabriel/bytebank/view/Home.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.fragment.app.viewModels
 import dev.joaogabriel.bytebank.R
+import dev.joaogabriel.bytebank.databinding.FragmentHomeBinding
+import dev.joaogabriel.bytebank.model.User
 import dev.joaogabriel.bytebank.utils.Resource
 import dev.joaogabriel.bytebank.viewmodel.UserViewModel
 
 class Home : Fragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
     private val userViewModel: UserViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentHomeBinding.bind(view)
 
         userViewModel.getUserData()
         userResponse()
@@ -21,12 +26,21 @@ class Home : Fragment(R.layout.fragment_home) {
     private fun userResponse() {
         userViewModel.userResponse.observe(viewLifecycleOwner, { response ->
             when(response) {
-                is Resource.Success -> println(response.data)
+                is Resource.Success -> setUserData(response.data)
 
                 is Resource.Error -> println(response.message)
 
                 is Resource.Loading -> println("loading sign in")
             }
         })
+    }
+
+    private fun setUserData(user: User?) {
+        if (user != null) {
+            val balance = String.format(resources.getString(R.string.txtBalance), user.balance)
+
+            binding.homeTxtName.text = user.name
+            binding.homeTxtBalance.text = balance
+        }
     }
 }
